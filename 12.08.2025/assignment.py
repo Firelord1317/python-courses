7a8228d7584394defa69a68627fcf5ae083f2a76
Firelord1317 <arnav.tyagi867@gmail.com>
2025-08-12 06:01:10 -0700

    commit

--- 12.08.2025/assignment.py
+++ 12.08.2025/assignment.py
@@ -7,6 +7,9 @@ friend5 = "6/10/2000"
 print("Hello, whose birth date would you like to view")
 friend = input("\nEnter the friend e.g friend1: ")
 
+# I have used two equal sins because one assigns value to the variable and two are use to compare values
+#I have used elif to check multiple conditions and else to handle the case when none of the conditions are met
+
 if friend == "friend1":
     print("Birth date of selected person is:", friend1)
 elif friend == "friend2":
